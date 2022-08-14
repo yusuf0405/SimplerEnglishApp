e2239c616b8c7dbc10b06e7c6dbe923b351ef6d1
Yusuf Ruziev <planetapluton888@gmail.com>
2022-08-14 13:17:19 +0300

    Rename MainActivity.kt class

--- simple-english-app/src/main/java/com/joseph/simplerapp/ActivityMain.kt
+++ simple-english-app/src/main/java/com/joseph/simplerapp/ActivityMain.kt
@@ -3,7 +3,7 @@ package com.joseph.simplerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
