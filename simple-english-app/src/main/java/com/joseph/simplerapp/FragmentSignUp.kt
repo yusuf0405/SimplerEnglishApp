696a48ff09819fe6105af63387a8f2b94c2820c3
Yusuf Ruziev <planetapluton888@gmail.com>
2022-08-16 22:34:25 +0300

    Add Sign uo classes

---
+++ simple-english-app/src/main/java/com/joseph/simplerapp/FragmentSignUp.kt
@@ -0,0 +1,31 @@
+package com.joseph.simplerapp
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentSignUp : Fragment() {
+
+    companion object {
+        fun newInstance() = FragmentSignUp()
+    }
+
+    private lateinit var viewModel: FragmentSignUpViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FragmentSignUpViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ simple-english-app/src/main/java/com/joseph/simplerapp/FragmentSignUpViewModel.kt
@@ -0,0 +1,7 @@
+package com.joseph.simplerapp
+
+import androidx.lifecycle.ViewModel
+
+class FragmentSignUpViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
