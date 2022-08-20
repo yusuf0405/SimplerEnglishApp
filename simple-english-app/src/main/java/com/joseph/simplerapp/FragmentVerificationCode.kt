23303942c8125ac9617001042ca32cf3e117cafd
Yusuf Ruziev <planetapluton888@gmail.com>
2022-08-20 23:19:42 +0300

    Add Verification Fragment

---
+++ simple-english-app/src/main/java/com/joseph/simplerapp/FragmentVerificationCode.kt
@@ -0,0 +1,31 @@
+package com.joseph.simplerapp
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentVerificationCode : Fragment() {
+
+    companion object {
+        fun newInstance() = FragmentVerificationCode()
+    }
+
+    private lateinit var viewModel: FragmentVerificationCodeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_verification_code, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FragmentVerificationCodeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ simple-english-app/src/main/java/com/joseph/simplerapp/FragmentVerificationCodeViewModel.kt
@@ -0,0 +1,7 @@
+package com.joseph.simplerapp
+
+import androidx.lifecycle.ViewModel
+
+class FragmentVerificationCodeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
