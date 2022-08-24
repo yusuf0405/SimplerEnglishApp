785e4c67e02042d0a08c0be97c23bc202914a816
Yusuf Ruziev <planetapluton888@gmail.com>
2022-08-24 21:53:43 +0300

    Add Verification classes

---
+++ simple-english-app/src/main/java/com/joseph/simplerapp/FragmentVerificationCode.kt
@@ -0,0 +1,29 @@
+package com.joseph.simplerapp
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentVerificationCode : Fragment() {
+
+
+
+    private lateinit var viewModel: FragmentVerificationCodeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_verefication_code, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FragmentVerificationCodeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
